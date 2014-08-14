aba14a189c94a7f1c4de2477931beccfed839e9a
Nick Ocampo <nick@kps3.com>
2014-08-13 22:12:57 -0700

    Ajax gets JSON file and writes out the names of employees

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,11 @@
-console.log('\'Allo \'Allo!');
-$.ajax('../../data/employees.json', {
-	type: 'GET',
-	success: function() {
-	}
+// Get Employee JSON
+$.getJSON( '../data/employees.json', function(employee) {
+	'use strict';
+	// Loop through JSON array
+	// Place an li for each name
+	$(employee).each(function(i) {
+		$('#slaves').append('<li class="available">' + employee[i].name + '</li>');
+	});
 });
+// Set li class as available or unavailable
+
